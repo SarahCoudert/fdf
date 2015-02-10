555be3ff08c86c0bef759ba35bf40e5860d5df8c
sarahcoudert <sarahcoudert@gmx.fr>
2015-02-10 20:20:23 +0100

    :D PONEY

--- Poney/main.c
+++ Poney/main.c
@@ -16,25 +16,9 @@
 #include "libft/includes/libft.h"
 #include <unistd.h>
 
-#define HEIGHT_SCREEN		640
-#define WIDTH_SCREEN		480
+#define HEIGHT_SCREEN		480
+#define WIDTH_SCREEN		640
 #define COLOR				32
-
-void		ft_pause(void)
-{
-	int		continuer;
-	SDL_Event		event;
-
-	continuer = 1;
-
-	while (continuer)
-	{
-		SDL_WaitEvent(&event);
-		if (event.type)
-			if (event.type == SDL_QUIT)
-				continuer = 0;
-	}
-}
 
 int		main(int ac, char **av)
 {
@@ -43,7 +27,14 @@ int		main(int ac, char **av)
 	SDL_Surface *screen;
 	SDL_Surface *bg;
 	SDL_Rect rect;
+	SDL_Surface *poney;
+	SDL_Rect pos_poney;
+	int		continuer;
+	SDL_Event		event;
 
+	continuer = 1;
+	pos_poney.x = 75;
+	pos_poney.y = 210;
 	rect.x = 0;
 	rect.y = 0;
 	bg = NULL;
@@ -52,17 +43,61 @@ int		main(int ac, char **av)
 	{
 		ft_putendl("SDL_Init error.");
 		return (-1);
-		}
+	}
 	(SDL_WM_SetIcon(SDL_LoadBMP("sdl_icone.bmp"), NULL));
-	screen = SDL_SetVideoMode(HEIGHT_SCREEN, WIDTH_SCREEN, COLOR, SDL_HWSURFACE);
+	screen = SDL_SetVideoMode(WIDTH_SCREEN, HEIGHT_SCREEN, COLOR, SDL_HWSURFACE | SDL_DOUBLEBUF);
+	SDL_Flip(screen);
 	if (screen == NULL)
 		ft_putendl_fd("Error : Cannot load video", 2);
 	SDL_WM_SetCaption("Horsemen of the Appocalypse", NULL);
 	if ((bg = SDL_LoadBMP("HD-Grass-Wallpapers.bmp")) == NULL)
 		ft_putendl_fd("Error : Cannot charge background image", 2);
+
 	SDL_BlitSurface(bg, NULL, screen, &rect);
-	SDL_Flip(screen);
-	ft_pause();
+	if ((poney = SDL_LoadBMP("chevalbmp.bmp")) == NULL)
+		ft_putendl_fd("Error : Cannot charge horse image", 2);
+
+	SDL_SetColorKey(poney, SDL_SRCCOLORKEY, SDL_MapRGB(poney->format, 255, 255, 255));
+
+	SDL_BlitSurface(poney, NULL, screen, &pos_poney);
+	SDL_EnableKeyRepeat(10, 10);
+	while (continuer)
+	{
+		SDL_WaitEvent(&event);
+		if (event.type)
+		{
+			if (event.type == SDL_QUIT)
+			{
+				continuer = 0;
+			}
+			else if (event.type == SDL_KEYDOWN)
+			{
+				if (event.key.keysym.sym == SDLK_ESCAPE)	// press escape quit the game
+					continuer = 0;
+				if (event.key.keysym.sym == SDLK_q)			// pressing q too
+					continuer = 0;
+				if (event.key.keysym.sym == SDLK_RIGHT)		// move poney to the right
+					if (pos_poney.x + 80 <= WIDTH_SCREEN)
+						pos_poney.x += 3;					//we move 3px by 3px to make it faster
+				if (event.key.keysym.sym == SDLK_LEFT)		// to the left
+					if (pos_poney.x - 3 >= 0)
+						pos_poney.x -= 3;
+				if (event.key.keysym.sym == SDLK_UP) 		//to the top
+					if (pos_poney.y - 3 >= 0)
+						pos_poney.y -= 3;
+				if (event.key.keysym.sym == SDLK_DOWN)		//to the bottom
+					if (pos_poney.y + 75 <= HEIGHT_SCREEN)
+						pos_poney.y += 3;
+			}
+			else
+				continuer = 1;
+		}
+		SDL_BlitSurface(bg, NULL, screen, &rect);
+		SDL_BlitSurface(poney, NULL, screen, &pos_poney);
+		SDL_Flip(screen);
+	}
+	SDL_FreeSurface(bg);
+	SDL_FreeSurface(poney);
 	SDL_Quit();
 	return (0);
 }
